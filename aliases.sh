7a5d8578ee4ccaf68f4ec70d8014648732337459
Dogus K. Bilir <85401781+doguskbilir@users.noreply.github.com>
2024-05-20 00:52:51 +0200

    Edit ls aliases

--- aliases.sh
+++ aliases.sh
@@ -12,15 +12,19 @@ alias ls='ls --color=auto'
 
 # List all files colorized in long format
 alias l='ls -lFh'
-alias ll='ls -lh'
-alias la='ls -A'
-alias lla='ls -lA'
+alias ll='ls -l'
+alias la='ls -lAh'
 alias lr='ls -R'
 
 # List only directories
 alias lsd='ls -l | grep "^d"'
 
-# List files sorted by last modification time
-alias lt='ls -lt'
-# List files sorted by last access time descending
-alias lu='ls -ltu'
+# List files sorted by last modification time (recent at top)
+alias lt='ls -ltA'
+# List files sorted by last modification time (recent at bottom)
+alias ltr='ls -ltAr'
+
+# List files sorted by last access time (recent at top)
+alias lu='ls -ltuA'
+# List files sorted by last access time (recent at bottom)
+alias lur='ls -ltuA'
